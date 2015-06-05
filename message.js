d59d1adea700059a5859c241e732f3a16c2be6d3
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-06-05 18:17:17 +0100

    Send email to back end. Next step: send the email!

---
+++ message.js
@@ -0,0 +1,33 @@
+var socket = new WebSocket("ws://localhost:8080");
+		
+socket.onopen = logOpen;
+socket.onmessage = logMessage;
+socket.onclose = logClose;
+		
+function logOpen() {
+	console.log("connected to WebSocket");
+}
+
+function logMessage(msg) {
+	console.log(msg);	   
+}
+
+function logClose() {
+	console.log("connection to WebSocket failed");
+}
+
+var Email = function(from, time, subject, body, to, cc, bcc){
+	this.from = from;
+	this.time = time;
+	this.subject = subject;
+	this.body = body;
+	this.cc = cc;
+	this.bcc = bcc;
+	this.to = to;
+	return this;
+}
+
+function sendMessage(type, message){
+	var item = {type: type, message: message};
+	socket.send(JSON.stringify(item));
+}
